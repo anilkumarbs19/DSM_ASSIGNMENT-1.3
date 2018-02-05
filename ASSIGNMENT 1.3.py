41303ce4b1b5049db873b169c21cc4649181f242
anilkumarbs19 <anilkumarbs19@gmail.com>
2018-02-05 20:59:58 +0530

    Assignment 1.3 DSM

---
+++ ASSIGNMENT 1.3.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[9]:
+
+
+FirstName = input ("Enter your First Name:")
+LastName = input ("Enter your Last Name:")
+FullName = FirstName + LastName
+print(FullName)   # Print the complete name received from the user
+Reverseorder = LastName + " " + FirstName
+print(Reverseorder)    #Print the Complete name in Reverse Order
+
